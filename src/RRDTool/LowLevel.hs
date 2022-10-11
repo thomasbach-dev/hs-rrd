150d2558e24e0d6ef0b9d3f9063c4b81f4d0020e
Thomas Bach <info@thomasbach.dev>
2022-10-11 21:18:30 +0200

    feat(capi): bindings for the error handlers of librrd

--- src/RRDTool/LowLevel.hs
+++ src/RRDTool/LowLevel.hs
@@ -2,6 +2,10 @@
 module RRDTool.LowLevel
   ( rrd_strversion
   , rrd_version
+    -- ** Error Handling
+  , rrd_clear_error
+  , rrd_test_error
+  , rrd_get_error
   ) where
 
 import Foreign.C.String (CString, peekCString)
@@ -12,3 +16,12 @@ rrd_strversion :: IO String
 rrd_strversion = peekCString raw_rrd_strversion
 
 foreign import capi "rrd.h rrd_version" rrd_version :: Double
+
+foreign import capi "rrd.h rrd_clear_error" rrd_clear_error :: IO ()
+
+foreign import capi "rrd.h rrd_test_error" rrd_test_error :: Int
+
+foreign import capi "rrd.h rrd_get_error" raw_rrd_get_error :: CString
+
+rrd_get_error :: IO String
+rrd_get_error = peekCString raw_rrd_get_error
--- test/RRDTool/LowLevelSpec.hs
+++ test/RRDTool/LowLevelSpec.hs
@@ -12,6 +12,21 @@ spec = do
     version <- runIO rrd_strversion
     it "returns the expected version of the library" $ do
       version `shouldBe` "1.7.2"
+
   describe "rrd_version" $ do
     it "returns the expected version of the library" $ do
       rrd_version `shouldBe` 1.7002
+
+  describe "rrd_clear_error" $ do
+    result <- runIO rrd_clear_error
+    it "just runs" $ do
+       result `shouldBe` ()
+
+  describe "rrd_get_error" $ do
+    result <- runIO rrd_get_error
+    it "returns an empty string" $ do
+      result `shouldBe` ""
+
+  describe "rrd_test_error" $ do
+    it "returns a 0" $ do
+      rrd_test_error `shouldBe` 0
